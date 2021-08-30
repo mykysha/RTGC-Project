3290a4f138fca3a457470035a6a8a811fa07b360
ndergunov <ndergunov@gmail.com>
2021-08-30 16:48:58 +0300

    HW2-Corrected1

--- api/handlers_ws.go
+++ api/handlers_ws.go
@@ -17,7 +17,12 @@ func (a *API) wsHandler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("ws fail: %v", err)
 	}
 	Sessions[ws] = true
-	log.Println("ws success")
+	log.Println("ws connection successfull")
 	ws.WriteMessage(websocket.TextMessage, []byte("Init correct"))
+	defer sessionClose(ws)
 	defer ws.Close()
+}
+
+func sessionClose(ws *websocket.Conn) {
+	Sessions[ws] = false
 }
