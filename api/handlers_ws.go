44f22cc174ba85d05c8b66e31db64b002ee29fdf
ndergunov <ndergunov@gmail.com>
2021-08-30 16:43:49 +0300

    HW2-Corrected

--- api/handlers_ws.go
+++ api/handlers_ws.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var Sessions = make(map[*websocket.Conn]bool)
+
 var upgrader = websocket.Upgrader{}
 
 func (a *API) wsHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,6 +16,7 @@ func (a *API) wsHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("ws fail: %v", err)
 	}
+	Sessions[ws] = true
 	log.Println("ws success")
 	ws.WriteMessage(websocket.TextMessage, []byte("Init correct"))
 	defer ws.Close()
